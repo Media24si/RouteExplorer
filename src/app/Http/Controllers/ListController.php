b093f38255aadff407145932bd1eb291c77eabd4
Bostjan Oblak <bostjan@muha.cc>
2015-04-19 22:03:16 +0200

    prepare for route testing

--- src/app/Http/Controllers/ListController.php
+++ src/app/Http/Controllers/ListController.php
@@ -1,10 +1,29 @@
 <?php namespace Media24si\RouteExplorer\app\Http\Controllers;
 
-class ListController extends Controller {
+use Illuminate\Foundation\Bus\DispatchesCommands;
+use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+
+use Illuminate\Http\Request;
+
+class ListController extends BaseController {
+	use DispatchesCommands, ValidatesRequests;
 
 	public function routes() {
-		$routes = \Route::getRoutes();
+		$routes = self::$router->getRoutes();
 		return view('RouteExplorer::list.routes', ['routes' => $routes]);
 	}
 
+
+	public function matchRoute() {
+		$path = '/routes';
+		$method = 'GET';
+
+		$request = Request::create($path, $method);
+
+		// match route or return 404 not found
+		$match = self::$router->getRoutes()->match($request);
+
+		dd($match);
+	}
 }
--- src/app/Http/routes.php
+++ src/app/Http/routes.php
@@ -1,3 +1,5 @@
 <?php
 
 Route::get('/routes', 'Media24si\RouteExplorer\app\Http\Controllers\ListController@routes');
+
+Route::get('/routes-test', 'Media24si\RouteExplorer\app\Http\Controllers\ListController@matchRoute');
