5b78e703b17824ca1fdee7745764d5032fba345a
Bostjan Oblak <bostjan@muha.cc>
2016-02-08 12:04:14 +0100

    laravel 5.1 lock, ready for tag

--- src/RouteExplorerServiceProvider.php
+++ src/RouteExplorerServiceProvider.php
@@ -4,32 +4,34 @@ namespace Media24si\RouteExplorer;
 
 use Illuminate\Support\ServiceProvider;
 
-class RouteExplorerServiceProvider extends ServiceProvider  {
+class RouteExplorerServiceProvider extends ServiceProvider
+{
 
-	/**
-	 * Bootstrap the application services.
-	 *
-	 * @return void
-	 */
-	public function boot()
-	{
-		include __DIR__ . '/app/Http/routes.php';
-	}
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        include __DIR__ . '/app/Http/routes.php';
+    }
 
-	/**
-	 * Register the application services.
-	 *
-	 * @return void
-	 */
-	public function register()
-	{
-		$this->registerViews();
-	}
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->registerViews();
+    }
 
-	/**
-	 * Register package views
-	 */
-	private function registerViews() {
-		$this->loadViewsFrom(__DIR__ . '/resources/views/', 'RouteExplorer');
-	}
+    /**
+     * Register package views
+     */
+    private function registerViews()
+    {
+        $this->loadViewsFrom(__DIR__ . '/resources/views/', 'RouteExplorer');
+    }
 }
--- src/app/Http/Controllers/ListController.php
+++ src/app/Http/Controllers/ListController.php
@@ -1,43 +1,47 @@
-<?php namespace Media24si\RouteExplorer\app\Http\Controllers;
+<?php 
+
+namespace Media24si\RouteExplorer\app\Http\Controllers;
 
 use Illuminate\Foundation\Bus\DispatchesCommands;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
-
 use Illuminate\Http\Request;
 
-class ListController extends BaseController {
-	use DispatchesCommands, ValidatesRequests;
+class ListController extends BaseController
+{
+    use DispatchesCommands, ValidatesRequests;
 
-	public function routes() {
-		$routes = self::$router->getRoutes();
-		return view('RouteExplorer::list.routes', ['routes' => $routes]);
-	}
+    public function routes()
+    {
+        $routes = self::$router->getRoutes();
+        return view('RouteExplorer::list.routes', ['routes' => $routes]);
+    }
 
 
-	public function matchRoute() {
-		$path = '/routes';
-		$method = 'GET';
+    public function matchRoute()
+    {
+        $path = '/routes';
+        $method = 'GET';
 
-		$request = Request::create($path, $method);
+        $request = Request::create($path, $method);
 
-		// match route or return 404 not found
-		$match = self::$router->getRoutes()->match($request);
+        // match route or return 404 not found
+        $match = self::$router->getRoutes()->match($request);
 
-		dd($match);
-	}
+        dd($match);
+    }
 
-	public function routeGroups() {
-		$routes = self::$router->getRoutes();
+    public function routeGroups()
+    {
+        $routes = self::$router->getRoutes();
 
-		foreach( $routes as $route )
-		{
-			$prefix = ($route->getPrefix() !== null) ? $route->getPrefix() : "/";
-			$routes_array[$prefix][] = $route;
-		}
+        foreach ($routes as $route) {
+            $prefix = ($route->getPrefix() !== null) ? $route->getPrefix() : "/";
+            $routes_array[$prefix][] = $route;
+        }
 
-		ksort($routes_array);
+        ksort($routes_array);
 
-		return view('RouteExplorer::list.route-groups', ['routes' => $routes_array]);
-	}
+        return view('RouteExplorer::list.route-groups', ['routes' => $routes_array]);
+    }
 }
